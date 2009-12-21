razorinc/virt-v2v

commit 9e3d11601be5d7e9e9be56def4d42afd2b789611

snapshot: Don't modify DOM in _foreach_disk
DOM was being modified in _foreach_disk, only because of what it was refactored
from.

snapshot/v2v-snapshot.pl
@@ -752,10 +752,6 @@ sub _foreach_disk
             my $item = $src_attrs->getNamedItem($attr);
             if(defined($item)) {
                 $path = $item->getNodeValue();
-
-                # Remove the attribute. We'll add a new one in below.
-                $src_attrs->removeNamedItem($attr);
-
                 last;
             }
         }

commit 38e0515cc8fa84c9b3571c0c5933a490a3c1af2a

snapshot: Explicitly remove unused disk source attribute
This fixes a regression introduced by 9e3d1160. Snapshot was leaving both dev
and file attributes specified. The result was that the v2v would actually
operate on the underlying storage rather than the snapshot.

snapshot/v2v-snapshot.pl
@@ -529,8 +529,10 @@ sub _commit_guest
         # Update the domain XML with the location of the backing store
         if($backing_type == Sys::Virt::StorageVol::TYPE_BLOCK) {
             $source->setAttribute('dev', $backing_path);
+            $source->removeAttribute('file');
         } else {
             $source->setAttribute('file', $backing_path);
+            $source->removeAttribute('dev');
         }
 
         # Update the domain XML with with a driver appropriate to the backing
@@ -646,6 +648,9 @@ sub _snapshot_guest
 
             # Update the source to be a "file" with the new path
             $source->setAttribute("file", $vol->get_path());
+
+            # Remove the dev attribute in case it was set
+            $source->removeAttribute("dev");
 
             # Also update the disk element to be a "file"
             $source->getParentNode()->setAttribute('type', 'file');