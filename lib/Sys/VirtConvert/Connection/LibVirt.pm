razorinc/virt-v2v

commit 7e9393129116699d1228bb02b9f65b78584582c1

Don't lose VNC password during conversion
If v2v was converting a libvirt guest which required a VNC password, the
converted libvirt guest would not require a VNC password. This change ensures we
don't lose the password during libvirt->libvirt conversion. RHEV doesn't have
anything directly analogous to this, so when converting libvirt->RHEV we display
a warning if the source guest required a VNC password.

Fixes CVE-2011-1773

lib/Sys/VirtConvert/Connection/LibVirt.pm
@@ -169,6 +169,23 @@ sub _parse_dom
         push(@{$meta{features}}, $feature->getNodeName());
     }
 
+    $meta{display} = {};
+    my ($display) = $root->findnodes('devices/graphics');
+    if (defined($display)) {
+        $meta{display}->{type} = _node_val($display, '@type');
+
+        my $password = _node_val($display, '@passwd');
+        $meta{display}->{password} = $password if defined($password);
+
+        my $keymap = _node_val($display, '@keymap');
+        $meta{display}->{keymap} = $keymap if defined($keymap);
+    }
+
+    # If there's no display element, default it to VNC with no password
+    else {
+        $meta{display}->{type} = 'vnc';
+    }
+
     $meta{disks} = [];
     foreach my $disk ($root->findnodes('devices/disk[@device=\'disk\']')) {
         my %info;

lib/Sys/VirtConvert/Connection/LibVirtSource.pm
@@ -243,7 +243,7 @@ sub _get_meta
     # Warn and exit if we didn't find it
     return undef unless(defined($domain));
 
-    my $dom = new XML::DOM::Parser->parse($domain->get_xml_description());
+    my $dom = new XML::DOM::Parser->parse($domain->get_xml_description(1));
     $self->{meta} = Sys::VirtConvert::Connection::LibVirt::_parse_dom($self,
                                                                       $dom);
 }

lib/Sys/VirtConvert/Connection/LibVirtTarget.pm
@@ -307,7 +307,6 @@ sub _meta_to_domxml
   <devices>
     <input type='tablet' bus='usb'/>
     <input type='mouse' bus='ps2'/>
-    <graphics type='vnc' port='-1' listen='127.0.0.1'/>
     <video>
       <model type='cirrus' vram='9216' heads='1'/>
     </video>
@@ -337,6 +336,14 @@ DOM
     my $nide = 0;
 
     my ($devices) = $root->findnodes('devices');
+
+    my $graphics = _append_elem($devices, 'graphics');
+    $graphics->setAttribute('type', $meta->{display}->{type});
+    $graphics->setAttribute('keymap', $meta->{display}->{keymap})
+        if (defined($meta->{display}->{keymap}));
+    $graphics->setAttribute('passwd', $meta->{display}->{password})
+        if (defined($meta->{display}->{password}));
+
     foreach my $disk (sort { $a->{device} cmp $b->{device} } @{$meta->{disks}})
     {
         my $is_block = $disk->{dst}->is_block();

lib/Sys/VirtConvert/Connection/RHEVTarget.pm
@@ -701,6 +701,12 @@ sub create_guest
 </ovf:Envelope>
 EOF
 
+    logmsg WARN, __('Guest specifies a password for connection to its '.
+                    'display, but this is not supported by RHEV. The '.
+                    'converted guest\'s display will not require a separate '.
+                    'password to connect.')
+        if exists($meta->{display}->{password});
+
     $self->_disks($ovf, $meta, $guestcaps);
     $self->_networks($ovf, $meta, $config, $guestcaps);
 

virt-v2v.spec.PL
@@ -82,6 +82,9 @@ Requires:  perl(:MODULE_COMPAT_%(eval "`%{__perl} -V:version`"; echo $version))
 Requires:       perl-libguestfs >= 1:1.6.0
 Requires:       perl-hivex >= 1.2.2
 
+# Required for passing flags to get_xml_description
+Requires:       perl(Sys::Virt) >= 0.2.4
+
 # Net::SSL is loaded dynamically by Net::HTTPS in
 # Sys::VirtConvert::Transfer::ESX. The dependency isn't automatically
 # discovered.

commit 1bdc5dd1454f19df4a30cf724e7de29728bbc164

Fix failure to convert a libvirt guest with no <graphics> element
This fixes a regression introduced by 7e939312. In particular, libvirt's ESX
driver doesn't produce a <graphics> element for the guest, meaning it wasn't
possible to convert ESX guests.

lib/Sys/VirtConvert/Connection/LibVirt.pm
@@ -169,9 +169,9 @@ sub _parse_dom
         push(@{$meta{features}}, $feature->getNodeName());
     }
 
-    $meta{display} = {};
     my ($display) = $root->findnodes('devices/graphics');
     if (defined($display)) {
+        $meta{display} = {};
         $meta{display}->{type} = _node_val($display, '@type');
 
         my $password = _node_val($display, '@passwd');