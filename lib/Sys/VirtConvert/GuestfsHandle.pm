razorinc/virt-v2v

commit 6dec2753fd9e7d726e140bb246ccf32df8af9fb5

Use device name hints when opening libguestfs handle
Allow inspection to succeed in situations where device names are not guessable,
for example when they include cciss devices.

lib/Sys/VirtConvert/GuestfsHandle.pm
@@ -22,7 +22,6 @@ use warnings;
 
 use Carp;
 
-use Sys::Guestfs::Lib qw(open_guest);
 use Sys::VirtConvert::Util qw(rhev_helper);
 
 use Locale::TextDomain 'virt-v2v';
@@ -55,18 +54,19 @@ close() method, and the ability to register pre-close callbacks.
 
 =over
 
-=item new(storage, transferiso, isrhev)
+=item new(disks, transferiso, isrhev)
 
 Create a new object. Open a new Sys::Guestfs handle to proxy, using the disks
-defined in the array I<storage>. Add I<transferiso> as a read-only drive if it
-is given. If I<isrhev> is true, the handle will use user and group 36:36.
+defined in I<disks>, which is taken from the guest metadata. Add I<transferiso>
+as a read-only drive if it is given. If I<isrhev> is true, the handle will use
+user and group 36:36.
 
 =cut
 
 sub new
 {
     my $class = shift;
-    my ($storage, $transfer, $isrhev) = @_;
+    my ($disks, $transfer, $isrhev) = @_;
 
     my $self = {};
 
@@ -75,10 +75,20 @@ sub new
     my $open = sub {
         my $interface = "ide";
 
-        $g = open_guest($storage, rw => 1, interface => $interface);
+        $g = Sys::Guestfs->new();
+        foreach my $disk (@{$disks}) {
+            my $vol = $disk->{dst};
+
+            $g->add_drive_opts($vol->get_path(),
+                               format => $vol->get_format(),
+                               iface => $interface,
+                               name => $disk->{device});
+        }
 
         # Add the transfer iso if there is one
-        $g->add_drive_ro_with_if($transfer, $interface) if (defined($transfer));
+        $g->add_drive_opts($transfer,
+                           format => 'raw', iface => $interface, readonly => 1)
+            if defined($transfer);
 
         # Enable networking in the guest
         $g->set_network(1);

p2v/server/virt-p2v-server.pl
@@ -336,14 +336,12 @@ sub convert
     die("CONVERT without prior SET_PROFILE command\n") unless defined($target);
     die("CONVERT without prior METADATA command\n") unless defined($meta);
 
-    my @localpaths = map { $_->{local_path} } @{$meta->{disks}};
-
     my $g;
     eval {
         my $transferiso = $config->get_transfer_iso();
 
         $g = new Sys::VirtConvert::GuestfsHandle(
-            \@localpaths,
+            $meta->{disks},
             $transferiso,
             $target->isa('Sys::VirtConvert::Connection::RHEVTarget')
         );

v2v/virt-v2v.pl
@@ -517,9 +517,8 @@ v2vdie __('Guest doesn\'t define any storage devices')
 $source->copy_storage($target, $output_format, $output_sparse);
 
 # Open a libguestfs handle on the guest's storage devices
-my @localpaths = map { $_->{dst}->get_local_path() } @{$meta->{disks}};
 my $g = new Sys::VirtConvert::GuestfsHandle(
-    \@localpaths,
+    $meta->{disks},
     $transferiso,
     $output_method eq 'rhev'
 );

virt-v2v.spec.PL
@@ -65,7 +65,6 @@ BuildRequires:  perl(Module::Pluggable)
 BuildRequires:  perl(Net::HTTPS)
 BuildRequires:  perl(Net::SSL)
 BuildRequires:  perl(Sys::Guestfs)
-BuildRequires:  perl(Sys::Guestfs::Lib)
 BuildRequires:  perl(Sys::Virt)
 BuildRequires:  perl(Term::ProgressBar)
 BuildRequires:  perl(URI)

commit 786a74f0f1c14cfb54a65ea8c5fc097f612b03a5

Fix crash during format conversions
Fix a regression introduced by commit 6dec2753fd9e7d726e140bb246ccf32df8af9fb5.
We didn't update the format conversion code when updating the GuestfsHandle API.
This change updates the API again, removing an unnecessary dependency between
GuestfsHandle and Volume.

lib/Sys/VirtConvert/GuestfsHandle.pm
@@ -57,9 +57,9 @@ close() method, and the ability to register pre-close callbacks.
 =item new(disks, transferiso, isrhev)
 
 Create a new object. Open a new Sys::Guestfs handle to proxy, using the disks
-defined in I<disks>, which is taken from the guest metadata. Add I<transferiso>
-as a read-only drive if it is given. If I<isrhev> is true, the handle will use
-user and group 36:36.
+defined in I<disks>, which is list of disk metadata defined as: [ [ name, path,
+format ], ... ]. Add I<transferiso> as a read-only drive if it is given. If
+I<isrhev> is true, the handle will use user and group 36:36.
 
 =cut
 
@@ -77,12 +77,12 @@ sub new
 
         $g = Sys::Guestfs->new();
         foreach my $disk (@{$disks}) {
-            my $vol = $disk->{dst};
+            my ($name, $path, $format) = @$disk;
 
-            $g->add_drive_opts($vol->get_path(),
-                               format => $vol->get_format(),
+            $g->add_drive_opts($path,
+                               format => $format,
                                iface => $interface,
-                               name => $disk->{device});
+                               name => $name);
         }
 
         # Add the transfer iso if there is one

lib/Sys/VirtConvert/Transfer/Local.pm
@@ -179,12 +179,13 @@ package Sys::VirtConvert::Transfer::GuestfsStream;
 sub new
 {
     my $class = shift;
-    my ($path) = @_;
+    my ($path, $format) = @_;
 
     my $self = {};
     bless($self, $class);
 
-    $self->{g} = new Sys::VirtConvert::GuestfsHandle([$path], undef, 0);
+    $self->{g} = new Sys::VirtConvert::GuestfsHandle([['sda', $path, $format]],
+                                                     undef, 0);
 
     return $self;
 }

p2v/server/virt-p2v-server.pl
@@ -343,8 +343,12 @@ sub convert
     eval {
         my $transferiso = $config->get_transfer_iso();
 
+        my @disks = map { [ $_->{device},
+                            $_->{dst}->get_path(),
+                            $_->{dst}->get_format() ] } @{$meta->{disks}};
+
         $g = new Sys::VirtConvert::GuestfsHandle(
-            $meta->{disks},
+            \@disks,
             $transferiso,
             $target->isa('Sys::VirtConvert::Connection::RHEVTarget')
         );

v2v/virt-v2v.pl
@@ -518,8 +518,12 @@ v2vdie __('Guest doesn\'t define any storage devices')
 $source->copy_storage($target, $output_format, $output_sparse);
 
 # Open a libguestfs handle on the guest's storage devices
+my @disks = map { [ $_->{device},
+                    $_->{dst}->get_path(),
+                    $_->{dst}->get_format() ] } @{$meta->{disks}};
+
 my $g = new Sys::VirtConvert::GuestfsHandle(
-    $meta->{disks},
+    \@disks,
     $transferiso,
     $output_method eq 'rhev'
 );