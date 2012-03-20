3de6a4d4bc8dc1eb80fde9e97d85dcd35127db41
Matthew Booth <mbooth@redhat.com>
2012-03-20 14:55:21 +0000

    p2v-client: Build with -Wall -Werror

--- p2v/client/ext/rblibssh2/extconf.rb
+++ p2v/client/ext/rblibssh2/extconf.rb
@@ -3,12 +3,11 @@ require 'mkmf'
 # Allow compiler to be overridden by the environment
 RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
 
-# XXX: Need to work out how to permanently enable -Werror
-#RbConfig::MAKEFILE_CONFIG['cflags'] = '-Werror'
-
 extension_name = 'rblibssh2'
 
 raise "libssh2 not found" unless pkg_config('libssh2')
 
-dir_config(extension_name)
-create_makefile(extension_name)
+with_cflags("-Wall -Werror -g -O2") do
+    dir_config(extension_name)
+    create_makefile(extension_name)
+end
