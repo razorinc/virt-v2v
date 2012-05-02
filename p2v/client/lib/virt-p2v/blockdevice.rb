razorinc/virt-v2v

commit fa10746132a63333a1eb5e2c0198ba490a5aba56

client: Ignore block devices with size 0
Dell iDRAC virtual devices are presented to the server as SCSI devices with size
0 if they don't have media connected. Attempting to read from them causes an
error due to missing medium. Excluding devices with size 0 seems generally sane.

Fixes RHBZ#816922

p2v/client/lib/virt-p2v/blockdevice.rb
@@ -17,6 +17,7 @@
 module VirtP2V
 
 class NoSuchDeviceError < StandardError; end
+class InvalidDevice < StandardError; end
 
 class FixedBlockDevice
     @@devices = {}
@@ -46,6 +47,8 @@ class FixedBlockDevice
         rescue Errno::ENOENT
             # Unlikely, but not fatal
         end
+
+        raise InvalidDevice if size == 0
 
         @device = device
         @size = size
@@ -108,7 +111,11 @@ begin
             devpath = dev.gsub("!", "/")
 
             if removable == "0" then
-                FixedBlockDevice.new(devpath)
+                begin
+                    FixedBlockDevice.new(devpath)
+                rescue InvalidDevice
+                    # Not fatal: ignore the device
+                end
             elsif File.exist?("/sys/block/#{dev}/device/modalias")
                 # Look in device/modalias to work out what kind of removable
                 # device this is
@@ -130,8 +137,11 @@ begin
                             { |type_f|
                                 type = type_f.gets.chomp
                                 # DISK or MOD
-                                if type == "0" || type == "7"
+                                if type == "0" || type == "7" then begin
                                     FixedBlockDevice.new(devpath)
+                                rescue InvalidDevice
+                                    # Not fatal: ignore the device
+                                end
 
                                 # WORM or ROM
                                 elsif type == "4" || type == "5"

commit d6ddbbfbfe754dd99b90ef99a70ffbc84826ba5b

p2v-client: Fix detection of cciss devices
This change fixes a regression introduced by
fa10746132a63333a1eb5e2c0198ba490a5aba56.

Resolves: RHBZ#818119

p2v/client/lib/virt-p2v/blockdevice.rb
@@ -50,7 +50,8 @@ class FixedBlockDevice
 
         raise InvalidDevice if size == 0
 
-        @device = device
+        # cciss device /dev/cciss/c0d0 will be cciss!c0d0 under /sys/block
+        @device = device.gsub("!", "/")
         @size = size
         @@devices[device] = self
     end
@@ -107,12 +108,9 @@ begin
         File.open("/sys/block/#{dev}/removable") { |fd|
             removable = fd.gets.chomp
 
-            # cciss device /dev/cciss/c0d0 will be cciss!c0d0 under /sys/block
-            devpath = dev.gsub("!", "/")
-
             if removable == "0" then
                 begin
-                    FixedBlockDevice.new(devpath)
+                    FixedBlockDevice.new(dev)
                 rescue InvalidDevice
                     # Not fatal: ignore the device
                 end
@@ -124,9 +122,9 @@ begin
                 { |modalias_f|
                     modalias = modalias_f.gets.chomp
                     if modalias =~ /floppy/ then
-                        RemovableBlockDevice.new(devpath, 'floppy')
+                        RemovableBlockDevice.new(dev, 'floppy')
                     elsif modalias =~ /cdrom/ then
-                        RemovableBlockDevice.new(devpath, 'cdrom')
+                        RemovableBlockDevice.new(dev, 'cdrom')
                     elsif modalias =~ /^scsi:t-/ then
                         # All this tells us is that we have a SCSI device: it
                         # could still be anything. Look at the device type to
@@ -138,27 +136,27 @@ begin
                                 type = type_f.gets.chomp
                                 # DISK or MOD
                                 if type == "0" || type == "7" then begin
-                                    FixedBlockDevice.new(devpath)
+                                    FixedBlockDevice.new(dev)
                                 rescue InvalidDevice
                                     # Not fatal: ignore the device
                                 end
 
                                 # WORM or ROM
                                 elsif type == "4" || type == "5"
-                                    RemovableBlockDevice.new(devpath, 'cdrom')
+                                    RemovableBlockDevice.new(dev, 'cdrom')
                                 else
-                                    ignore_device(devpath)
+                                    ignore_device(dev)
                                 end
                             }
                         rescue Errno::ENOENT
-                            ignore_unknown_device(devpath)
+                            ignore_unknown_device(dev)
                         end
                     else
-                        ignore_unknown_device(devpath)
+                        ignore_unknown_device(dev)
                     end
                 }
             else
-                ignore_unknown_device(devpath)
+                ignore_unknown_device(dev)
             end
         }
     }