razorinc/virt-v2v

commit bcd46673ca3c4259ff23c9c9954adb80f77ceacc

Handle libvirt pools whose format differs from qemu's format
Only 3 of the 10 libvirt pool types have a 'format' which has the same meaning
as in qemu. In all the rest, we need to ensure we don't specify or propagate a
meaningless format.

Fixes RHBZ#803629

lib/Sys/VirtConvert/Connection/LibVirtSource.pm
@@ -158,6 +158,37 @@ sub get_volume
         ($name, $format, $size, $usage, $is_sparse, $is_block) =
             parse_libvirt_volinfo($vol);
 
+        # The retrieved format is only meaningful for a subset of pool types.
+        # Check which one we've got.
+        my $pool;
+        eval {
+            # get_storage_pool_by_volume was omitted from Sys::Virt up to and
+            # including version 0.10.0. If it's not available, we use an
+            # internal function instead.
+            if ($vol->can('get_storage_pool_by_volume')) {
+                $pool = $self->{vmm}->get_storage_pool_by_volume($vol);
+            } else {
+                $pool = Sys::Virt::StoragePool::_lookup_by_volume($vol);
+            }
+        };
+        v2vdie __x('Failed to retrieve storage pool for volume {path}: {error}',
+                   path=> $path, error => $@) if $@;
+
+        my $poolxml = $pool->get_xml_description();
+        my $dom = new XML::DOM::Parser->parse($pool->get_xml_description());
+        my ($pooltype) = $dom->findnodes('/pool/@type');
+        if (defined($pooltype)) {
+            $pooltype = $pooltype->getNodeValue();
+
+            $format = 'raw'
+                unless $Sys::VirtConvert::Libvirt::format_pools{$pooltype};
+        } else { # Should be impossible
+            logmsg WARN, __x('Pool XML has no type attribute: {xml}',
+                             xml => $pool->get_xml_description());
+
+            $format = 'raw';
+        }
+
         $transfer = $self->_get_transfer($name, $path, $format,
                                          $is_sparse, $is_block);
     }

lib/Sys/VirtConvert/Connection/LibVirtTarget.pm
@@ -28,6 +28,8 @@ use Sys::VirtConvert::Connection::LibVirt;
 use Sys::VirtConvert::Connection::Volume;
 use Sys::VirtConvert::Util qw(:DEFAULT parse_libvirt_volinfo);
 
+use XML::DOM;
+
 use Locale::TextDomain 'virt-v2v';
 
 @Sys::VirtConvert::Connection::LibVirtTarget::ISA =
@@ -143,7 +145,8 @@ sub create_volume
     push(@cleanup_vols, [$pool, $name]);
 
     my $allocation = $sparse ? 0 : $size;
-    my $vol_xml = "
+
+    my $vol_xml_format = "
         <volume>
             <name>$name</name>
             <capacity>$size</capacity>
@@ -153,6 +156,44 @@ sub create_volume
             </target>
         </volume>
     ";
+
+    my $vol_xml_noformat = "
+        <volume>
+            <name>$name</name>
+            <capacity>$size</capacity>
+            <allocation>$allocation</allocation>
+        </volume>
+    ";
+
+    # Only certain pool types support different file formats. Check the pool
+    # type to determine whether we should create the volume with an explicit
+    # format, or rely on the pool default.
+    my $vol_xml;
+    my $pooldom = new XML::DOM::Parser->parse($pool->get_xml_description());
+    my ($pooltype) = $pooldom->findnodes('/pool/@type');
+    if (defined($pooltype)) {
+        $pooltype = $pooltype->getNodeValue();
+
+        if ($Sys::VirtConvert::Libvirt::format_pools{$pooltype}) {
+            $vol_xml = $vol_xml_format;
+        } else {
+            $vol_xml = $vol_xml_noformat;
+
+            # If the target format type isn't raw, warn the user that they're
+            # not getting what they expected
+            logmsg WARN, __x('Target pool type {pooltype} doesn\'t support '.
+                             'format {format}',
+                             pooltype => $pooltype, format => $format)
+                unless $format eq 'raw';
+
+            $format = 'raw';
+        }
+    } else { # Should be impossible
+        logmsg WARN, __x('Pool XML has no type attribute: {xml}',
+                         xml => $pool->get_xml_description());
+
+        $vol_xml = $vol_xml_noformat;
+    }
 
     my $vol;
     eval {

lib/Sys/VirtConvert/Connection/LibVirt.pm
@@ -34,6 +34,18 @@ use Sys::VirtConvert::Util;
 
 use Locale::TextDomain 'virt-v2v';
 
+# As of writing (20/09/2012), libvirt supports 10 pool types. Of these, only th
+# 3 below use the format element of a volume to indicate the on-disk data
+# format. 6 don't support the format element at all, and 1 other, disk,
+# overloads it to mean partition type.
+# All pool types other than the 3 below effectively only support raw.
+our %format_pools = (
+    dir => 1,
+    fs => 1,
+    netfs => 1
+);
+
+
 =pod
 
 =head1 NAME

commit 12964120db69c1328cda1104755218067b1f0f72

Fix regression introduced by bcd46673ca3c4259ff23c9c9954adb80f77ceacc
Incorrect use of format_pools meant that all libvirt volumes were marked as
raw.

lib/Sys/VirtConvert/Connection/LibVirtSource.pm
@@ -181,7 +181,7 @@ sub get_volume
             $pooltype = $pooltype->getNodeValue();
 
             $format = 'raw'
-                unless $Sys::VirtConvert::Libvirt::format_pools{$pooltype};
+                unless $Sys::VirtConvert::Connection::LibVirt::format_pools{$pooltype};
         } else { # Should be impossible
             logmsg WARN, __x('Pool XML has no type attribute: {xml}',
                              xml => $pool->get_xml_description());

lib/Sys/VirtConvert/Connection/LibVirtTarget.pm
@@ -174,7 +174,7 @@ sub create_volume
     if (defined($pooltype)) {
         $pooltype = $pooltype->getNodeValue();
 
-        if ($Sys::VirtConvert::Libvirt::format_pools{$pooltype}) {
+        if ($Sys::VirtConvert::Connection::LibVirt::format_pools{$pooltype}) {
             $vol_xml = $vol_xml_format;
         } else {
             $vol_xml = $vol_xml_noformat;